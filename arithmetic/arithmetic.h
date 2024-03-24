5f6057801dcfae73b38c02d8575b37fa69656a77
jasinco <jasinc90@gmail.com>
2024-03-24 19:32:20 +0800

    finish arithmetic header file
    finish a simple arithmetic header file with the support of + - * /
    and with a c file to demonstrate the usage

--- arithmetic/arithmetic.h
+++ arithmetic/arithmetic.h
@@ -5,25 +5,35 @@
 
 struct unit{
     char opr;
-    int num;
+    float num;
 };
 
 // x greater or equal to y
 bool ge(char x, char y){
     // if it is * or /, it must be the greatest operator in this case.
-    // and or x==y means that if both x and y is '+' or '-', it will return equal
-    return x=='*' || x == '/' || x==y;
+    return (x=='*' || x == '/') || !(y=='*' || y == '/'); 
 }
 
-int calc(char* equation){
+bool is_opr(struct unit x){
+    return x.num == 0 && (x.opr == '/' || x.opr == '*' || x.opr == '+' || x.opr == '-');
+}
+bool is_opr_char(char x){
+    return (x == '/' || x == '*' || x == '+' || x == '-');
+}
+
+
+float calc(char* equation){
     struct unit *units = calloc(51, sizeof(struct unit));
     int unitslen = 0;
 
     char *opr = calloc(50, sizeof(char));
     int oprlen = 0;
-    int answer = 0;
+    float answer = 0;
 
+    //Parse to reverse polish notation
     //Shunting Yard Algorithm
+    
+    int delta = 1; // manage that if the number is a negative number
     for (int i = 0; i<100;i++){
         if (equation[i] == 0x00){
             break;
@@ -41,14 +51,21 @@ int calc(char* equation){
                     // The units was reseted to 0 so it represent the upper one 
                     // and just append a 0 into the stack
                     unitslen++;
+                    opr[oprlen] = equation[i];
                     oprlen++;
                     break;
                 }
 
+                // update units length to another unit
                 unitslen++;
-
+                delta = 1;
                 if (oprlen > 0){
                     while (oprlen != 0){
+                        if (equation[i] == '-' && is_opr_char(equation[i-1])){
+                                delta = -1;
+                                unitslen--;
+                                break;
+                        }
                         if (ge(opr[oprlen-1], equation[i])){
                             units[unitslen].opr = opr[oprlen-1];
                             unitslen++;
@@ -58,8 +75,10 @@ int calc(char* equation){
                         break;
                     }
                 }
-                opr[oprlen] = equation[i];
-                oprlen++;
+                if (delta  == 1){
+                    opr[oprlen] = equation[i];
+                    oprlen++;
+                }
                 //check append
                 //negative two statement
                 // in the first of this array 
@@ -67,7 +86,7 @@ int calc(char* equation){
                 break;
             default:
                 struct unit *u =  &units[unitslen]; 
-                u->num = u->num * 10 + (equation[i]-'0');
+                u->num = u->num * (float)10 + (float)(equation[i]-'0') * (float)delta;
                 break;
         }
     }
@@ -76,10 +95,41 @@ int calc(char* equation){
         unitslen++;
         units[unitslen].opr  = opr[oprlen-1];
     }
+    unitslen++;
+    free(opr);
+
+    // caculate the result 
+    for (int i = 0; i < unitslen; i++){
+        if (is_opr(units[i]) && i > 1 && !(is_opr(units[i-1]) || is_opr(units[i-2]))){
+            float result = 0;
+            switch (units[i].opr){
+                case '+':
+                    result = units[i-2].num + units[i-1].num;
+                    break;
+                case '-':
+                    result = units[i-2].num - units[i-1].num;
+                    break;
+                case '*':
+                    result = units[i-2].num * units[i-1].num;
+                    break;
+                case '/':
+                    result = units[i-2].num / units[i-1].num;
+                    break;
+            }
+            struct unit pending;
+            pending.num = result;
+            pending.opr = '\0';
+            units[i-2] =  pending;
+            for (int j = i+1;j < unitslen; j++){
+                units[j-2] = units[j];
+            }
+            unitslen -= 2;
+            i -= 4;
+        }
+    }
 
     answer = units[0].num;
     free(units);
-    free(opr);
     return answer; 
 }
 
--- arithmetic/test.c
+++ arithmetic/test.c
@@ -3,6 +3,6 @@
 #include "string.h"
 
 int main(void){
-    static char* equation = "-100+2";
-    printf("%d\n", calc(equation));
+    static char* equation = "100-2131+21323*231/2323";
+    printf("%.2f\n", calc(equation));
 }
